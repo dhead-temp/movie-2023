f53ecd8f8a861a6e998234d2859cfbbd6533b501
digiheadway <99395515+digiheadway@users.noreply.github.com>
2023-10-23 14:24:47 +0530

    img updated

--- inc/php/data.php
+++ inc/php/data.php
@@ -6,19 +6,19 @@ $anchor1 = 'Avatar';
 $hero_link1 = 'Avatar: The Way of Water';
 
 //Hero Item 2
-$img2 = 'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC45LzEwICAxMDIuOUsgdm90ZXM%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00331997-lmpkuaycqf-portrait.jpg';
-$anchor2 = 'Drishyam 2';
-$hero_link2 = 'Drishyam 2';
+$img2 = 'https://assets.gadgets360cdn.com/pricee/assets/product/202206/Jawan-poster_1655912386.jpg';
+$anchor2 = 'Jawan';
+$hero_link2 = 'Jawan';
 
 //Hero Item 3
-$img3 = 'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ny44LzEwICAzNi4ySyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00312739-twjrkftutt-portrait.jpg';
-$anchor3 = 'Bhediya';
-$hero_link3 = 'v';
+$img3 = 'https://m.media-amazon.com/images/M/MV5BYTYzN2RiZDMtOWQ2YS00YmVkLTk3NmYtZWU4NTBkNWMwZGNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_.jpg';
+$anchor3 = 'Ganapath';
+$hero_link3 = 'Ganapath';
 
 //Hero Item 4
-$img4 = 'https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-Ni41LzEwICAyLjlLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00345156-hgsdravwle-portrait.jpg';
-$anchor4 = 'Connect';
-$hero_link4 = 'Connect';
+$img4 = 'https://static.moviecrow.com/gallery/20230921/221109-Leo%20Vijay%20Sanjay%20Dutt%20Poster.jpg';
+$anchor4 = 'Leo';
+$hero_link4 = 'Leo';
 
 
 #Latest Movie
